bc58499078fe45b80e607ff873f504f8bc421077
dynamicalsystemslaboratory <59177421+dynamicalsystemslaboratory@users.noreply.github.com>
2023-04-12 12:43:07 -0400

    Add files via upload

---
+++ Assets/bench_pos.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bench_pos : MonoBehaviour
+{
+    float dist;
+    public GameObject bench;
+    public GameObject[] benches;
+    GameObject bench1;
+    int count = 1;
+    int index;
+    bool bolflag = true;
+    ArrayList used = new ArrayList();
+
+    Vector3[] locations;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        locations = new Vector3[6];
+        benches = new GameObject[3];
+        locations[0] = new Vector3(65f, -24.9f, -51.32f);
+
+        locations[1] = new Vector3(66.2f, -24.9f, -31.5f);
+        locations[2] = new Vector3(99.7f, -24.9f, 10.5f);
+        locations[3] = new Vector3(99.7f, -24.9f, 23.5f);
+
+        locations[4] = new Vector3(99.7f, -24.9f, 97.3f);
+        locations[5] = new Vector3(99.7f, -24.9f, 115.8f);
+
+
+
+        print("location" + locations[0]);
+
+        // rock1.transform.position = locations[0];
+        index = (int)Random.Range(0, 6);
+        print(index);
+        used.Add(index);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // var position = new Vector3(Random.Range(-16, 10), 0, Random.Range(-12, 9));
+        // var radius = Random.Range(0, 2);
+
+
+        if (bolflag == true)
+        {
+            for (int n = 0; n < 3; n++)
+            {
+                index = (int)Random.Range(0, 5);
+                while (used.Contains(index) == true)
+                {
+                    print("same!!");
+                    index = (int)Random.Range(0, 5);
+                }
+                used.Add(index);
+                bench1 = bench;
+                benches[n] = Instantiate(bench, locations[index], Quaternion.identity);
+               // benches[n].transform.Rotate(0, 180f, 0);
+
+                print(index);
+                //rockfirst.SetActive(false);
+                count += 1;
+
+            }
+            bolflag = false;
+        }
+
+
+
+
+    }
+}
+
+
+
---
+++ Assets/ray1.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+//using Uduino;
+
+public class ray1 : MonoBehaviour
+{
+    StateController _statecontroller;
+   
+    private void Start()
+    {
+
+        _statecontroller = new StateController();
+        _statecontroller.eyes = this.transform;
+
+       
+    }
+
+    private void Update()
+    {
+        Look(_statecontroller);
+    }
+    //
+    //  LookDecision
+    //
+    public class StateController
+    {
+        public Transform eyes;
+        public Transform chaseTarget;
+
+    }
+    public class defaultStats
+    {
+        public static float lookAngle = 120;
+        public static float lookAccurate = 1;
+        public static float lookRange = 10f;
+    }
+    //放射线检测
+    private void Look(StateController controller)
+    {
+
+        float subAngle = (defaultStats.lookAngle / 2) / defaultStats.lookAccurate;
+
+
+       
+
+
+        LookAround(controller, Quaternion.identity, Color.green, transform.name + 0);
+        for (int i = 0; i < defaultStats.lookAccurate; i++)
+        {
+            LookAround(controller, Quaternion.Euler(0, -1 * subAngle * (i + 1), 0), Color.green, transform.name + (i + 1));
+            LookAround(controller, Quaternion.Euler(0, subAngle * (i + 1), 0), Color.green, transform.name + (i + 8));
+
+        }
+
+  
+
+  
+
+       
+
+
+
+
+
+    }
+
+    //check if there is player in ray
+    static public bool LookAround(StateController controller, Quaternion eulerAnger, Color DebugColor, string RayNumber)
+    {
+
+
+
+        var dir = eulerAnger * controller.eyes.forward.normalized * defaultStats.lookRange;
+        Debug.DrawRay(controller.eyes.position, dir, DebugColor);
+
+        RaycastHit hit;
+        if (Physics.Raycast(controller.eyes.position, eulerAnger * controller.eyes.forward, out hit, defaultStats.lookRange) && hit.collider.CompareTag("Player"))
+        {
+            controller.chaseTarget = hit.transform;
+            // Debug.Log("Found:" + hit.transform.name + "  RayNumber:" + RayNumber+"distance:"+hit.distance);
+
+
+
+
+            return true;
+        }
+
+        else
+        {
+
+            return false;
+
+        }
+
+    }
+
+}
